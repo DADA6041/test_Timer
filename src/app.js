a300b14b26e74ee058cc6f7db67a2e3f1bec9334
DADAKIM <d.ya96v@gmail.com>
2022-11-08 11:48:53 +0900

    edit_selector

--- src/app.js
+++ src/app.js
@@ -2,4 +2,5 @@ const txtHrs = document.querySelector('.txt-hrs');
 const txtMin = document.querySelector('.txt-min');
 const txtSec = document.querySelector('.txt-sec');
 
-const startBtn = document.querySelector('')
+const startBtn = document.querySelector('.btn-start');
+const resetBtn = document.querySelector('.btn-reset');
