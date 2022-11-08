db1e763e2571256c80aaee9500ab32444b84495f
DADAKIM <d.ya96v@gmail.com>
2022-11-08 11:42:52 +0900

    Timer_for_test

---
+++ src/app.js
@@ -0,0 +1,5 @@
+const txtHrs = document.querySelector('.txt-hrs');
+const txtMin = document.querySelector('.txt-min');
+const txtSec = document.querySelector('.txt-sec');
+
+const startBtn = document.querySelector('')
